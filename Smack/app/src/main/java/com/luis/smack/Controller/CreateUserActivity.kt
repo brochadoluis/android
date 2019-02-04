ece5442a32f7121d783deb5aca28fc2987a8ef84
Luís <brochadoluis@gmail.com>
2019-02-04 12:39:36 +0000

    add spinner add fields verifications add broadcast send

--- Smack/app/src/main/java/com/luis/smack/Controller/CreateUserActivity.kt
+++ Smack/app/src/main/java/com/luis/smack/Controller/CreateUserActivity.kt
@@ -1,12 +1,16 @@
 package com.luis.smack.Controller
 
+import android.content.Intent
 import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.LocalBroadcastManager
 import android.view.View
+import android.widget.Toast
 import com.android.volley.toolbox.Volley
 import com.luis.smack.R
 import com.luis.smack.Services.AuthService
+import com.luis.smack.Utilities.BROADCAST_USER_DATA_CHANGE
 import kotlinx.android.synthetic.main.activity_create_user.*
 import java.util.*
 
@@ -18,6 +22,7 @@ class CreateUserActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_user)
+        createSpinner.visibility = View.INVISIBLE
     }
 
     fun generateUserAvatar(view: View) {
@@ -49,20 +54,51 @@ class CreateUserActivity : AppCompatActivity() {
     }
 
     fun createUserClicked(view: View) {
+        enableSpinner(true)
         val username = createUsernameText.text.toString()
         val email = createEmailText.text.toString()
         val password = createPasswordText.text.toString()
 
-        AuthService.register(this, email, password) {
-            registerSuccess -> if(registerSuccess) {
+        if (username.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty()) {
+            AuthService.register(this, email, password) {
+                    registerSuccess -> if(registerSuccess) {
                 AuthService.login(this, email, password) {
-                    loginSuccess -> if(loginSuccess) {
-                        AuthService.createUser(this, username, email, userAvatar, avatarColor){ createSuccess ->
-                            if(createSuccess) finish()
+                        loginSuccess -> if(loginSuccess) {
+                    AuthService.createUser(this, username, email, userAvatar, avatarColor){ createSuccess ->
+                        if(createSuccess) {
+                            val userDataChange: Intent = Intent(BROADCAST_USER_DATA_CHANGE)
+                            LocalBroadcastManager.getInstance(this).sendBroadcast(userDataChange)
+                            enableSpinner(false)
+                            finish()
+                        } else {
+                            errorToast()
                         }
                     }
+                } else {
+                    errorToast()
                 }
+                }
+            } else {
+                errorToast("Make sure username, email and password are filled in.")
+            }
             }
         }
+    }
+
+    fun enableSpinner(enable: Boolean) {
+        if(enable){
+            createSpinner.visibility = View.VISIBLE
+        } else {
+            createSpinner.visibility = View.INVISIBLE
+        }
+
+        createUserBtn.isEnabled = !enable
+        createAvatarImageView.isEnabled = !enable
+        backgroundColorBtn.isEnabled = !enable
+    }
+
+    fun errorToast(errorMessage: String = "Something went wrong, please try again."){
+        Toast.makeText(this, errorMessage, Toast.LENGTH_LONG).show()
+        enableSpinner(false)
     }
 }
--- Smack/app/src/main/java/com/luis/smack/Utilities/Constants.kt
+++ Smack/app/src/main/java/com/luis/smack/Utilities/Constants.kt
@@ -1,6 +1,12 @@
 package com.luis.smack.Utilities
 
+// URL constants
+
 const val BASE_URL = "https://smack-slack-clone.herokuapp.com/v1/"
 const val URL_REGISTER = "${BASE_URL}account/register"
 const val URL_LOGIN = "${BASE_URL}account/login"
 const val URL_CREATE_USER = "${BASE_URL}user/add"
+
+// Broadcast constants
+
+const val BROADCAST_USER_DATA_CHANGE = "BROADCAST_USER_DATA_CHANGE"
