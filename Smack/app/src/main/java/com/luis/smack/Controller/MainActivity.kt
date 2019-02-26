a606f61fc467b1a756b8d5015f253e7d395af866
Luís <ei11009@fe.up.pt>
2019-02-26 16:18:25 +0000

    add display messages functionality

--- Smack/app/src/main/java/com/luis/smack/Controller/MainActivity.kt
+++ Smack/app/src/main/java/com/luis/smack/Controller/MainActivity.kt
@@ -11,10 +11,14 @@ import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.ArrayAdapter
 import android.widget.EditText
+import android.widget.LinearLayout
+import com.luis.smack.Adapters.MessageAdapter
 import com.luis.smack.Model.Channel
 import com.luis.smack.Model.Message
 import com.luis.smack.R
@@ -34,11 +38,17 @@ class MainActivity : AppCompatActivity() {
 
     val socket = IO.socket(SOCKET_URL)
     lateinit var channelAdapter: ArrayAdapter<Channel>
+    lateinit var messageAdapter: MessageAdapter
     var selectedChannel: Channel? = null
 
     private fun setupAdapters() {
         channelAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, MessageService.channels)
         channel_list.adapter = channelAdapter
+
+        messageAdapter = MessageAdapter(this, MessageService.messages)
+        messageListView.adapter = messageAdapter
+        val layoutManager = LinearLayoutManager(this)
+        messageListView.layoutManager = layoutManager
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -106,11 +116,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun updateWithChannel() {
-        mainChannelName.text = "${selectedChannel?.name}"
+        mainChannelName.text = "#${selectedChannel?.name}"
         if (selectedChannel != null) {
             MessageService.getMessages(selectedChannel!!.id) { complete ->
                 if(complete) {
-                    // show messages
+                    messageAdapter.notifyDataSetChanged()
+                    if(messageAdapter.itemCount > 0) {
+                        messageListView.smoothScrollToPosition(messageAdapter.itemCount-1)
+                    }
                 }
             }
         }
@@ -128,6 +141,8 @@ class MainActivity : AppCompatActivity() {
 
         if (App.prefs.isLoggedIn) {
             UserDataService.logout()
+            channelAdapter.notifyDataSetChanged()
+            messageAdapter.notifyDataSetChanged()
             usernameNavHeader.text = ""
             userEmailNavHeader.text = ""
             userImageNavHeader.setImageResource(R.drawable.profiledefault)
@@ -189,6 +204,8 @@ class MainActivity : AppCompatActivity() {
 
                     val newMessage = Message(messageBody, username, channelId, userAvatar, userAvatarColor, id, timestamp)
                     MessageService.messages.add(newMessage)
+                    messageAdapter.notifyDataSetChanged()
+                    messageListView.smoothScrollToPosition(messageAdapter.itemCount-1)
                 }
             }
         }
